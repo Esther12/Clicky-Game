074fb25fe766a311ca771d774d60d779230efb63
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-01 22:15:38 -0400

    Updates

---
+++ precache-manifest.f3903dbbb5c4f084e3b28f4f9df5fbab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8ea9f8caba080fd01658d534dd3429",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "03f35a348fffc8f2b333",
+    "url": "/Clicky-Game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cd65778a53a5ab20ce46",
+    "url": "/Clicky-Game/static/js/2.33649e61.chunk.js"
+  },
+  {
+    "revision": "03f35a348fffc8f2b333",
+    "url": "/Clicky-Game/static/js/main.a1e8fc0a.chunk.js"
+  },
+  {
+    "revision": "75fa83e2a97bc1c69d72",
+    "url": "/Clicky-Game/static/js/runtime~main.4267543b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
