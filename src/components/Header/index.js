89f992000fd3e1fc2a8b0bcc965d3d300fc969dd
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-06 15:16:40 -0400

    Change the path of the bgImg

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./style.css";
+import Bgimg from "../../sponge-island-Header.jpg";
 
 function Title(props) {
   return (
-    <div className="image-bg">
+    <div className="image-bg" style={{  backgroundImage : `url(${Bgimg})`}}>
         <div className="heads-bg d-flex align-items-center">
             <div className="mx-auto stickyDiv">
                     <h1 className="text-center">{props.children}</h1>
